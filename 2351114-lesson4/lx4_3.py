592abc8b25ee56ba1ad4ada853cc3fdb08a75d93
tj-messi <2715978522@qq.com>
2024-08-05 22:04:01 +0800

    1

--- 2351114-lesson4/lx4_3.py
+++ 2351114-lesson4/lx4_3.py
@@ -18,66 +18,46 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-class tj_bbs:
-    def __init__(self, wait_time=3):
-        self.bbs_browser = webdriver.Chrome()
-        self.post_page = 'http://cyr985.net3v.club/bbs/topic.asp?id=4891&boardid=6&TB=1'
-        self.wait_time = wait_time
+class TJ_BBS():
+    bbs_browser = webdriver.Chrome()
+    login_page = 'http://cyr985.net3v.club/bbs/login.asp'
+    post_page = 'http://cyr985.net3v.club/bbs/topic.asp?id=4891&boardid=6&TB=1'
+    wait_time = 5
 
-    def auto_login(self, username, password):
+    def auto_login(self, username: str, password: str):
+        self.bbs_browser.get(self.login_page)
+        time.sleep(self.wait_time)
+
+        name_input = WebDriverWait(self.bbs_browser, self.wait_time).until(
+            EC.presence_of_element_located((By.NAME, 'name'))
+        )
+        password_input = self.bbs_browser.find_element(By.NAME, 'Password')
+        name_input.send_keys(username)
+        password_input.send_keys(password)
+        submit_button = self.bbs_browser.find_element(By.CLASS_NAME, 'login')
+        submit_button.click()
+        time.sleep(self.wait_time)
+
+    def auto_post(self, bbs_content):
         self.bbs_browser.get(self.post_page)
-        try:
-            # 等待“登录”按钮可见并点击
-            login_button = WebDriverWait(self.bbs_browser, self.wait_time).until(
-                EC.element_to_be_clickable((By.XPATH, '//input[@type="submit" and @value="登陆"]'))
-            )
-            login_button.click()
-            # 等待用户名和密码输入框可见
-            username_input = WebDriverWait(self.bbs_browser, self.wait_time).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, '//label[contains(text(), "请输入您的用户名")]/following-sibling::input'))
-            )
-            password_input = self.bbs_browser.find_element(By.XPATH,
-                                                           '//label[contains(text(), "请输入您的密码")]/following-sibling::input')
-
-            # 输入用户名和密码
-            username_input.send_keys(username)
-            password_input.send_keys(password)
-
-            # 等待“登录”按钮可见并点击
-            login_button = WebDriverWait(self.bbs_browser, self.wait_time).until(
-                EC.element_to_be_clickable((By.XPATH, '//input[@type="submit" and @value="登陆"]'))
-            )
-            login_button.click()
-
-            # 等待页面加载
-            time.sleep(self.wait_time)
-        except Exception as e:
-            print(f"Error during login: {e}")
-
-    def auto_post(self):
-        try:
-            # 等待回复框可见
-            reply_box = WebDriverWait(self.bbs_browser, self.wait_time).until(
-                EC.presence_of_element_located((By.NAME, 'content'))
-            )
-
-            # 输入回复内容
-            reply_box.send_keys('2351114 zhu jun ze')
-
-            # 点击“ok发表”按钮
-            submit_button = self.bbs_browser.find_element(By.CSS_SELECTOR, 'input[type="submit"][value="ok发表"]')
-            submit_button.click()
-
-            # 等待页面加载
-            time.sleep(self.wait_time)
-        except Exception as e:
-            print(f"Error during posting: {e}")
+        time.sleep(self.wait_time)
+        reply_bt = self.bbs_browser.find_element(by=By.XPATH, value='//*[@id="edit"]')
+        reply_bt.send_keys(bbs_content)
+        time.sleep(self.wait_time)
+        sub_bt = self.bbs_browser.find_element(by=By.XPATH, value='//*[@id="sayb"]')
+        sub_bt.click()
+        time.sleep(self.wait_time)
+        pass
 
 
-# 示例使用
+def main():
+    tj = TJ_BBS()
+    username = "tj_messi"  # 替换为实际的用户名
+    password = "1398212202"  # 替换为实际的密码
+    tj.auto_login(username, password)
+    tj.auto_post("2351114 zhu jun ze")
+
+
 if __name__ == "__main__":
-    bbs = tj_bbs(wait_time=3)
-    bbs.auto_login('tj_messi', '1398212202')
-    bbs.auto_post()
-    bbs.bbs_browser.quit()
+    main()
+
