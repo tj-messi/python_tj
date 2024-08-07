b3a983615870243a04aee4b671fda0adb1ad42e1
tj-messi <2715978522@qq.com>
2024-08-07 16:10:17 +0800

    Create lx5_3.py

---
+++ 2351114-lesson5/lx5_3.py
@@ -0,0 +1,69 @@
+# 2351114 朱俊泽 大数据科学与技术
+'''
+参考https://blog.csdn.net/yjw123456/article/details/103811112完成minst手写数字识别
+如果安装keras失败，可试试：
+pip install keras -i https://pypi.tuna.tsinghua.edu.cn/simple/
+如果安装pandas失败，可试
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pandas 
+要求：训练和测试精度超过百分之90
+'''
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+# 加载MNIST数据集
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# 根据不同的后端设置输入形状
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, 28, 28)
+    x_test = x_test.reshape(x_test.shape[0], 1, 28, 28)
+    input_shape = (1, 28, 28)
+else:
+    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+    input_shape = (28, 28, 1)
+
+# 将像素值缩放到0到1之间
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# 将标签转换为one-hot编码
+num_classes = 10
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# 构建模型
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# 编译模型
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+# 训练模型
+batch_size = 128
+epochs = 12
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+# 评估模型
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
